5e97e31cb4b6885891552be0d7803c7cb4d05154
unknown <ysdevn@gmail.com>
2024-06-05 14:03:45 +0530

    for loop

--- JS-CLASS 1PM/4j_nestedcondition.js
+++ JS-CLASS 1PM/4j_nestedcondition.js
@@ -42,37 +42,68 @@
 // let entrance_exam="pass";
 // let addmssion="complited"
 
-let stu_name="shivani";
-if (stu_name){
-    console.log(stu_name+" has applied for B-tech addmssion");
+// let stu_name="shivani";
+// if (stu_name){
+//     console.log(stu_name+" has applied for B-tech addmssion");
 
-    let matrix_12=70;
-    if(matrix_12>=60){
-        console.log(stu_name+" has passed 12th class");
+//     let matrix_12=70;
+//     if(matrix_12>=60){
+//         console.log(stu_name+" has passed 12th class");
 
-        let stream="PCM";
-        if(stream=="PCM"){
-            console.log(stu_name+" has PCM stream eligble for entrance exam.");
+//         let stream="PCM";
+//         if(stream=="PCM"){
+//             console.log(stu_name+" has PCM stream eligble for entrance exam.");
 
-            let entrance_exam="pass"
-            if(entrance_exam=="pass"){
-                console.log(stu_name+" has eligble for addmission");
-            }
-            else{
-                console.log(stu_name+" has not eligble for addmission");
-            }
-        }
-        else{
-            console.log(stu_name+" has not PCM stream not eligble for entrance exam.");
-        }
+//             let entrance_exam="pass"
+//             if(entrance_exam=="pass"){
+//                 console.log(stu_name+" has eligble for addmission");
+//             }
+//             else{
+//                 console.log(stu_name+" has not eligble for addmission");
+//             }
+//         }
+//         else{
+//             console.log(stu_name+" has not PCM stream not eligble for entrance exam.");
+//         }
 
-    }
-    else{
-        console.log(stu_name+" has not passed 12th class");
-    }
-}
-else{
-    console.log("Student name is not provided");
-    console.log("Student name is not provided2");
-}
+//     }
+//     else{
+//         console.log(stu_name+" has not passed 12th class");
+//     }
+// }
+// else{
+//     console.log("Student name is not provided");
+//     console.log("Student name is not provided2");
+// }
 
+
+
+// Condition on string.
+
+// let str1="a"
+// let str2="A"
+// if(str1===str2){
+//     console.log("Both strings are same");
+// }
+// else{
+//     console.log("Both strings are not same");
+// }
+
+
+// let str1="aditya"
+// let str2="aev"
+// if(str1>str2){
+//     console.log("str1 is greater than str2");
+// }
+// else{
+//     console.log("str2 is greater than str1");
+// }
+
+// let str1="a"
+// let str2="1"
+// if(str1>str2){
+//     console.log("str1 is greater than str2");
+// }
+// else{
+//     console.log("str2 is greater than str1");
+// }

0cf3b2225c4603bc8e9393fb2bb1e17111514473
unknown <ysdevn@gmail.com>
2024-06-12 14:05:17 +0530

    12 june-1

---
+++ JS-CLASS 1PM/testing.js
@@ -0,0 +1,12 @@
+
+function submit1(){
+    let name_i=document.getElementById('full_name').value;
+    let email_i=document.getElementById('email').value;
+    let address_i=document.getElementById('address').value;
+        console.log(name_i)
+        console.log(email_i)
+        console.log(address_i)
+    //   document.getElementById('name1').innerHTML=name_i
+    //   document.getElementById('email').innerHTML=email_i
+    //   document.getElementById('address').innerHTML=address_i
+  }

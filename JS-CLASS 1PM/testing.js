09c6026cd1c8af74a5d0969e78e18c374af8a22d
unknown <ysdevn@gmail.com>
2024-06-17 13:50:40 +0530

    Do it-1

--- JS-CLASS 1PM/testing.js
+++ JS-CLASS 1PM/testing.js
@@ -1,12 +1,19 @@
+// let i=10 //initialization
+// while(i>0){
+//     console.log(i)
 
-function submit1(){
-    let name_i=document.getElementById('full_name').value;
-    let email_i=document.getElementById('email').value;
-    let address_i=document.getElementById('address').value;
-        console.log(name_i)
-        console.log(email_i)
-        console.log(address_i)
-    //   document.getElementById('name1').innerHTML=name_i
-    //   document.getElementById('email').innerHTML=email_i
-    //   document.getElementById('address').innerHTML=address_i
-  }
+// i--; //increament / decreament
+// }
+
+console.log("-------------------------------")
+
+let c=0 //initialization
+while(c<=10){
+    if (c==3){
+        continue
+    }
+    console.log(c)
+    c++
+
+// c++//increament / decreament
+}

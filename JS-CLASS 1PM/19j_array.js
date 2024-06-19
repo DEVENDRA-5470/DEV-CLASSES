d0b910c68b4467710b7dd39748fc9a468ec8e693
unknown <ysdevn@gmail.com>
2024-06-19 14:05:39 +0530

    array-19

---
+++ JS-CLASS 1PM/19j_array.js
@@ -0,0 +1,42 @@
+// Properties of arrary
+// Creation of arrary
+// Accessing of array
+// Modifying of array
+// Traversing of array
+// In-builted methods of array
+// Array structuring
+// Array destructuring
+// Assignments
+
+// Properties :
+// 1.Array is data structure used to store multiple value of different types of data.
+// 2.Array is mutable ,means use can change elment of array after creating also.
+// 3.Array support indexing and length and its index start from 0.
+// 4.In javascript, arrays are Dynamic.
+// 5.Array have many in-builted methods.
+
+// Creation of array :
+// 1.using  square([])
+// let stu_name=['shivani','babita','abhinav','aaditya','dev']
+// console.log(stu_name)
+
+// // 1.using  new keyword Array()
+// let new_stu = new Array('shivani','babita','abhinav','aaditya','dev')
+// console.log(new_stu)
+
+
+
+// ---------------- Accessing of elements
+// let stu_name=['shivani','babita','abhinav','aaditya','dev'];
+// let data=stu_name[(stu_name.length)-1];
+// let i=stu_name.length;
+// let data1=stu_name[i];
+// console.log(data1);
+
+
+// ---------------- Modification of elements
+// let stu_name=['shivani','babita','abhinav','aaditya','dev'];
+// stu_name[1]=1000
+// stu_name[2]="Dreamer Infotech"
+// console.log(stu_name);
+

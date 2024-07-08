862cd5ed3e1024a195252c07a0ddac83c6b6d092
unknown <ysdevn@gmail.com>
2024-07-08 13:29:14 +0530

    July-8

--- JS-CLASS 1PM/testing.js
+++ JS-CLASS 1PM/testing.js
@@ -1,10 +1,41 @@
-        function random_color(){
-            let char='0123456789ABCDEF';
-            let code='#'
+let ar=['Dreamer','infotech']
+let s=""
+for(let a of ar){
+    s=s+a
+}
 
-            for (let i=0 ; i<6 ; i++){
-                code=code+char[Math.floor(Math.random()*16)]
-            }
-            console.log(code)
+let all_v=''
+let all_c=''
+let v='aeiou'
+for(let t of s){
+    if (v.includes(t)){
+        all_v=all_v+t
+    }
+    else{
+        all_c=all_c+t
+    }
+
+}
+console.log(all_v)
+console.log(all_c)
+
+
+// find vowel and consonants
+let ar=['Dreamer','infotech']
+
+let newstr=String(ar)
+let str=newstr.split('')
+let v='aeiou'
+let all_vowel=[]
+let all_consonant=[]
+for(let a=0;a<str.length ;a++){
+    if (v.includes(str[a])){
+        all_vowel.push(str[a])
+    }
+    else{
+    
+        all_consonant.push(str[a])
         }
-        random_color()
+}
+console.log(all_vowel)
+console.log(all_consonant)

6254d279e195cdcba92e942aa627d6c779bc7249
unknown <ysdevn@gmail.com>
2024-07-23 13:07:12 +0530

    July-23

--- JS-CLASS 1PM/22_jul.map.js
+++ JS-CLASS 1PM/22_jul.map.js
@@ -20,6 +20,19 @@
 // let res=price.map(double_price)
 // console.log(res)
 
-let price=[2,4,6,7,8,9]
-let res=price.map(n=>n*2)
+// let price=[2,4,6,7,8,9]
+// let res=price.map(n=>n*2)
+// console.log(res)
+
+let array=["shivani","Dev","Abhinav","aaditiya", "aarav","ankita","kajal"];
+function filter_name(n){
+   for(i of n){
+    for (v in i){
+        if(i[0]=="s"){
+            return true;
+        }
+    }
+   }
+}
+let res=array.filter(filter_name)
 console.log(res)

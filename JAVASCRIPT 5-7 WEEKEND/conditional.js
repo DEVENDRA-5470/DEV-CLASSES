fe0b1d797e1f0b20700e8fed2145d9739ce0f81d
Syntax Error ❌ <ysdevn@gmail.com>
2024-08-17 17:57:01 +0530

    FIRST DJANGO

---
+++ JAVASCRIPT 5-7 WEEKEND/conditional.js
@@ -0,0 +1,14 @@
+// let a=10;
+// let b=20;
+// if(a<b){
+//     console.log("How are you")
+// }
+// else{
+//     console.log("NOT BAD")
+// }
+
+// 3.Logical operator (and(&&),or(||),not(!))
+let a=10
+let b=60
+let res= !(b==60 && a==10 && a>60 )
+console.log(res)

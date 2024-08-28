e948fe10287126605687fe740f90732695167fce
Syntax Error ❌ <ysdevn@gmail.com>
2024-08-28 14:04:41 +0530

    AUGUST-28

---
+++ JS-CLASS 1PM/DOM/10_promises.js
@@ -0,0 +1,20 @@
+// A promise in javascript is like a guarantee that something will happen
+// in the future.
+// It represents a task (like getting data from a server) that is happening
+// in the background.
+
+// A Promise Can have three phases:
+// 1.Pending : it will waiting to finish.
+// 2.Resloved(Fullfilled) : The task completed sucessfully.
+// 3.Reject  : The task failed.
+
+
+// Think of it as ordering food at a restaurant :
+// 1.You place your order (Promise is created).
+// 2.While you wait other things can happen (Promise is pending).
+// 3.You get your food (the promise is fullfilled) Or something goes wrong (the promise is rejected).
+
+// You can use .then() to do something when the promise fullfilled.
+// and .catch() to handle errors if the promise rejected.
+
+
---
+++ JS-CLASS 1PM/DOM/sync_async.js
@@ -0,0 +1,12 @@
+function user1(){
+    console.log("I am user 1")
+}
+function user2(){
+    console.log("I AM USER 2")
+}
+function user3(){
+    console.log("i am user 3")
+}
+user1()
+setTimeout(user2,3000)
+user3()

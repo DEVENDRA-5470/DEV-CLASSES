496b1c8ff40ab838c46a472235a952171f402282
DEV <ysdevn@gmail.com>
2024-09-28 18:41:17 +0530

    dev

---
+++ JAVASCRIPT 5-7 WEEKEND/11.function.js
@@ -0,0 +1,39 @@
+// // arguments
+// // parameters
+
+// let odd=0
+// let even=0
+// let s=0;
+// function fun(a){
+//     for (let i=0 ; i<a.length; i++){
+//         s+=a[i]
+//         if(a[i]%2==0){
+//             even+=1
+//         }
+//         else{
+//             odd+=1
+//         }
+
+//     }
+// }
+// let ar=[1,2,3,4,5]
+// fun(ar)
+// console.log(odd)
+// console.log(even)
+// console.log(s)
+
+
+// 1.Take something return nothing
+
+// function fun(a){
+//     console.log("Hello",a)
+// }
+// fun("Dev")
+
+// 1.Take something return something
+
+function fun(a){
+    return "Hello "+a
+}
+let res=fun("Dev")
+console.log(res)

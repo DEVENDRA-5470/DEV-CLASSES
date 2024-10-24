cfa86b10a09c3886c044c458c3cf5dba62fa6dba
DEV <ysdevn@gmail.com>
2024-10-24 13:57:37 +0530

    Its Dev

--- VISHESH CLASSES/15_while_loop.cpp
+++ VISHESH CLASSES/15_while_loop.cpp
@@ -2,12 +2,19 @@
 #include <string>
 using namespace std;
 
+// initialization
+// condition
+// increament/decrement
+
 void print_one_to_ten(){
 
-    for (int i=1 ; i<=10 ;i++ ){
+    int i=1; //initialization
+    while (i<=10){ //condition
+        i++;
         cout << i << endl;
     }
 }
+
 
 void print_ten_to_one(){
 
@@ -17,8 +24,8 @@ void print_ten_to_one(){
 }
 
 void print_steps(){
-
-    for (int i=1 ; i<=10 ;i+=2 ){
+    int i;
+    for (i=1 ; i<=10 ;i+=2 ){
         cout << i << endl;
     }
 }
@@ -55,11 +62,11 @@ void sum_product(){
 
 
 int main(){
-    //print_one_to_ten();
+    print_one_to_ten();
     //cout << "REVERSE 10 TO 1" << endl;
     //print_ten_to_one();
     //cout << "STEPS OF 2" << endl;
     //print_steps();
     //nested_loop();
-    sum_product();
+    //sum_product();
 }

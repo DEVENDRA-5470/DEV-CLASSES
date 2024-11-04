8466556ad1a0083ee84c7760dc146ddd5d65ac4f
DEV <ysdevn@gmail.com>
2024-11-04 15:47:46 +0530

    its Dev

--- VISHESH CLASSES/18_pointer.cpp
+++ VISHESH CLASSES/18_pointer.cpp
@@ -3,19 +3,18 @@
 #include <iostream>
 using namespace std;
 
-int var_address(){
-    int n=30;
-    int m=10;
-    int *p; // Declare
-    int *p1;
-    p=&n;
-    p1=&m;
+int var_address() {
+    int n = 30;
+    int m = 10;
+    int* p; // Declare
+    int* p1;
+    p = &n;
+    p1 = &m;
     cout << p << endl;
     cout << p1 << endl;
 }
 
 
-int main(){
+int main() {
     var_address();
 }
-

c3ab727009d149b69b658325514447770e54e7e4
DEVENDRA-5470 <your_email@example.com>
2024-12-11 13:26:49 +0530

    its dev

--- VISHESH CLASSES/25_Array.cpp
+++ VISHESH CLASSES/25_Array.cpp
@@ -17,11 +17,58 @@ int my_array(){
         cout << "Result :" << marks_10th[i] << endl;
     }
 
-    // sum of array
-    // product of array
-    // find odd even from array
+    // sum of array.
+    // product of array.
+    // find odd even from array.
+}
+
+int check_odd_even(){
+    int marks_10th[11]={100,200,37,49,50,60,45,98,67,88,99};
+
+    for (int i=0 ; i<11 ; i++){
+
+        if (marks_10th[i]%2==0){
+        cout << marks_10th[i] << " : EVEN" << endl;
+        }
+        else{
+        cout << marks_10th[i] << " : ODD" << endl;
+        }
+
+    }
+}
+
+void new_array(){
+    int new_arr[]={23,45,67,90,67,45,78,93,89,99,66,44};
+    int arr_size=sizeof(new_arr)/sizeof(new_arr[0]);
+    cout << arr_size << endl;
+
+    for (int i=0; i<arr_size ; i++){
+      cout << new_arr[i] << endl;
+    }
+}
+
+void input_array(){
+    int i=0;
+    int numbers[]={7};
+
+    for (int i=0; i<10 ; i++){
+      i=i+1;
+      cout << sizeof(numbers) << endl;
+    }
+
+
+
+
 }
 
 int main(){
-    my_array();
+    //my_array();
+    //check_odd_even();
+    //new_array();
+    // input_array();
 }
+
+// array
+// string
+// oops
+// complete.

de1dd59dd53a0f046cd4e29128c59e89c456d51d
DEVENDRA-5470 <your_email@example.com>
2025-03-25 09:16:43 +0530

    mar-24

---
+++ VISHESH CLASSES/JAVASCRIPT/23_mar.js
@@ -0,0 +1,50 @@
+// Variables
+// var , let , const.: these keyword used to declare variable in Javascript.
+// 1.Scope
+// var :
+//  Function scoped , it available throughout the function its declared in or globally
+// if declared outside a function .
+// it ignores block scope
+
+// let :
+// Blocked-Scope
+
+// const:
+// Blocked-Scope
+
+// Example 
+
+
+// if (true){
+//     var x = 10;
+//     let y = 12;
+//     const z = 10
+//     console.log(x,"in")
+//     console.log(y,"in")
+//     console.log(z,"in")
+// }
+
+// console.log(x,"out")
+// console.log(y)
+// console.log(z)
+
+// 2.Reassignment.
+// var a = 20;
+// var a = 30;
+// var a=  50;
+// console.log(a)
+
+// let not support reassignment / but we can use redeclaration
+// let b =20;
+// let b =30;
+// console.log(b)
+
+// let b; // declared
+// b=30
+// b=50
+// console.log(b)
+
+// const a=40;
+// console.log(a)
+
+

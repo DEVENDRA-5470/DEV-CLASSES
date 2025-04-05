8a9894ad9d1e3a4fdbb1ac99bd58e7ad41835809
DEVENDRA-5470 <your_email@example.com>
2025-04-05 13:46:58 +0530

    apr-5

--- DRF-API/TODOAPP/my_app/api/urls.py
+++ DRF-API/TODOAPP/my_app/api/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
-from .views import *
+from my_app.api.views import *
 urlpatterns = [
-    path("all-task/",all_task),
-    path("task/<int:pk>/",task)
+    # path("",all_task),
+    # path("task/<int:pk>/",task),
+    # path("delete_task/<int:pk>/",delete_task),
+    # path("update_task/<int:pk>/",partial_update),
+    # path("full_update/<int:pk>/",full_update),
+    # path("create_data/",create_data),
+
+    # class based view
+    path("all-task/",All_task.as_view()),
+    path("get-task/<int:pk>",get_task.as_view()),
+    path("delete-task/<int:pk>",delete_task.as_view()),
+    path("create-task/",create_task.as_view()),
+    path("update-task/<int:pk>",update_task.as_view()),
+    path("all-action/<int:pk>",all_action.as_view()),
+    
 ]
--- DRF-API/TODOAPP/my_app/api/views.py
+++ DRF-API/TODOAPP/my_app/api/views.py
@@ -3,22 +3,102 @@ from rest_framework.response import Response
 from rest_framework import status
 from my_app.api.serializers import My_tode_ser
 from my_app.models import My_todo
+from rest_framework import generics,permissions
 
 
-@api_view(["GET"])
-def all_task(request):
-    data=My_todo.objects.all()
-    ser=My_tode_ser(data,many=True)
-    return Response(ser.data)
+# @api_view(["GET"])
+# def all_task(request):
+#     data=My_todo.objects.all()
+#     ser=My_tode_ser(data,many=True)
+#     return Response(ser.data)
 
+# @api_view(["GET"])
+# def task(request,pk):
+#     try:
+#         data=My_todo.objects.get(pk=pk)
 
-@api_view(["GET"])
-def task(request,pk):
-    try:
-        data=My_todo.objects.get(pk=pk)
-
-    except My_todo.DoesNotExist:
-        return Response({"error":"Data is not found"},status=404)
+#     except My_todo.DoesNotExist:
+#         return Response({"error":"Data is not found"},status=404)
     
-    ser=My_tode_ser(data)
-    return Response(ser.data)
+#     ser=My_tode_ser(data)
+#     return Response(ser.data)
+
+# @api_view(["DELETE"])
+# def delete_task(request,pk):
+#     try:
+#         data=My_todo.objects.get(pk=pk)
+#         data.delete()
+#         return Response({"Ok":"Data is Deleted"},status=200)
+
+#     except My_todo.DoesNotExist:
+#         return Response({"error":"Data is not found"},status=404)
+
+
+# @api_view(['PATCH'])
+# def partial_update(request,pk):
+#     try:
+#         my_data=My_todo.objects.get(pk=pk)
+#     except My_todo.DoesNotExist:
+#         return Response({"error":"Data is not found"},status=404)
+    
+#     ser=My_tode_ser(my_data,data=request.data ,partial=True)
+#     if ser.is_valid():
+#         ser.save()
+#         return Response(ser.data ,status=200)
+#     return Response(ser.errors ,status=400)
+
+# @api_view(['PUT'])
+# def full_update(request,pk):
+#     try:
+#         my_data=My_todo.objects.get(pk=pk)
+#     except My_todo.DoesNotExist:
+#         return Response({"error":"Data is not found"},status=404)
+    
+#     ser=My_tode_ser(my_data,data=request.data)
+#     if ser.is_valid():
+#         ser.save()
+#         return Response(ser.data ,status=200)
+#     return Response(ser.errors ,status=400)
+
+
+# @api_view(['POST'])
+# def create_data(request):
+#     ser=My_tode_ser(data=request.data , partial=True)
+#     if ser.is_valid():
+#         ser.save()
+#         return Response(ser.data ,status=200)
+#     return Response(ser.errors ,status=400)
+
+
+
+# CLASS BASED RESOURCES
+
+class All_task(generics.ListAPIView):
+    queryset = My_todo.objects.all()
+    serializer_class = My_tode_ser
+
+class get_task(generics.RetrieveAPIView):
+    queryset = My_todo.objects.all()
+    serializer_class = My_tode_ser
+    lookup_field="pk"
+
+class delete_task(generics.DestroyAPIView):
+    queryset = My_todo.objects.all()
+    serializer_class = My_tode_ser
+    lookup_field="pk"
+    permission_classes=[permissions.IsAuthenticatedOrReadOnly]
+
+
+class create_task(generics.CreateAPIView):
+    queryset = My_todo.objects.all()
+    serializer_class = My_tode_ser
+    
+class update_task(generics.UpdateAPIView):
+    queryset = My_todo.objects.all()
+    serializer_class = My_tode_ser
+    lookup_field="pk"
+
+class all_action(generics.RetrieveUpdateDestroyAPIView):
+    queryset = My_todo.objects.all()
+    serializer_class = My_tode_ser
+    lookup_field="pk"

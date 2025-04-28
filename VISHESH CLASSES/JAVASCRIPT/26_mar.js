43e31b7550710b1b96ed32dbeba6d0951adb0e02
DEVENDRA-5470 <your_email@example.com>
2025-04-28 12:57:53 +0530

    apr-28

--- VISHESH CLASSES/JAVASCRIPT/26_mar.js
+++ VISHESH CLASSES/JAVASCRIPT/26_mar.js
@@ -4,3 +4,8 @@ let res=a+b;
 console.log(`Result ✅✅✅✅ : ${res}`)
 
 // 1.w
+
+// Write a program for addition of 3 numbers
+// Write a program for multiplicaton of 4 numbers
+// Write a program for division of 10 and 5 numbers.
+// Write a program for subtraction of two numbers.

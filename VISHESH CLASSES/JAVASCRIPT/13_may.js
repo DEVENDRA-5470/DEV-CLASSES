ff2fd43cebd6733c2789acafc4a16672fd9bac46
DEVENDRA-5470 <your_email@example.com>
2025-05-24 13:39:56 +0530

    may-24

--- VISHESH CLASSES/JAVASCRIPT/13_may.js
+++ VISHESH CLASSES/JAVASCRIPT/13_may.js
@@ -6,29 +6,67 @@
 
 
 
-// Array Defination
-// Array Creation
-// Array Updation
-// Array Traversion
-// Array in-built methods
+// Array Defination.
+// Array Creation.
+// Array Updation.
+// Array Traversion.
+// Array in-built methods.
 
 // Array Defination.
 // Array is a data structure used to store mutltiple data of different types in one variable.
 
 // Array Creation.
-let stu_marks=[12,23,50,90,98]
-// length_count form 1
-// index_cont form 0
+// let mark = new Array()
+// mark=[78,90,90]
+// let stu_marks=[12,23,50,90,98]
+// length_count form 1.
+// index_cont form 0.
 // last_index = len - 1
-console.log(stu_marks.length)
-let a=stu_marks.length
-console.log(stu_marks[0])
-console.log(stu_marks[4])
-console.log(stu_marks[a-1])
-console.log("BEFORE",stu_marks)
+// console.log(stu_marks.length)
+// let a=stu_marks.length
+// console.log(stu_marks[0])
+// console.log(stu_marks[4])
+// console.log(stu_marks[a-1])
+// console.log("BEFORE",stu_marks)
+
 //Array Update
-stu_marks[4]="Aman"
-stu_marks[0]=['Dev','Visesh','React']
-console.log("AFTER",stu_marks)
-console.log(stu_marks[0][1])
+// stu_marks[4]="Aman"
+// stu_marks[0]=['Dev','Visesh','React']
+// console.log("AFTER",stu_marks)
+// console.log(stu_marks[0][1])
+
+// Array Traverse. (for loop, while loop,do while loop,for in loop,for of loop,foreach loop)
+
+let data=new Array()
+
+// data=['Dev',"Vishesh",67,90,30]
+
+// 1.for loop
+// for (let i=0 ; i<data.length ; i++){
+    //     console.log(i)
+    // }
+    // console.log(data)
+
+
+ // 2.while loop
+// let i=0;
+// while (i<data.length){
+    //     console.log(data[i])
+    //     i++;
+    // }
+        
+
+// 3.for of loop
+// for (i of data){
+//     console.log(i)
+// }
+
+// 4.for in loop
+// for (i in data){
+//     console.log(data[i])
+// }
+
+
+// Wap to find out the all even number from the given array.
+let my_arr=[40,23,20,45,6,90,23,21,30]
 
